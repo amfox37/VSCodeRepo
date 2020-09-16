d353d6a19fda7d41d92f5dfa4934c46605e82b0d
amfox37 <amfox37@gmail.com>
2020-09-15 18:02:17 -0600

    working on arrays

---
+++ projects/cherno_tutorials/video_31.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    /* code */
+    int example[5];
+    example[0] = 2; // Example is actually an integer pointer
+    example[4] = 3;
+
+    for (int i = 0; i < 5; i++)
+    {
+        example[i] = 2;
+    }
+    
+    
+    std::cout << example[4] << std::endl;
+
+    std::cin.get();
+}
