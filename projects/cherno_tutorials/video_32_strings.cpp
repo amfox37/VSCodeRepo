6c5ce3fa55f98d511b38a82e813866b8863e023c
amfox37 <amfox37@gmail.com>
2020-09-15 18:24:54 -0600

    This is what you'd do in C

--- projects/cherno_tutorials/video_32_strings.cpp
+++ projects/cherno_tutorials/video_32_strings.cpp
@@ -3,6 +3,10 @@
 int main()
 {
     const char* name = "Cherno";
+    char name2[7] = {'C', 'h', 'e', 'r', 'n', 'o', '\0'};
+
+    std::cout << name << std::endl;
+    std::cout << name2 << std::endl;
 
     std::cin.get();
 }
