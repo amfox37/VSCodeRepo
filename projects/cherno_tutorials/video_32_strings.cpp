1384c573df5e4f8a73b1d9e81d90059380b0e7e1
amfox37 <amfox37@gmail.com>
2020-09-16 18:55:23 -0600

    string literals...

--- projects/cherno_tutorials/video_32_strings.cpp
+++ projects/cherno_tutorials/video_32_strings.cpp
@@ -6,8 +6,13 @@ int main()
     std::string name = "Cherno"; // Char pointer of course
     char name2[7] = {'C', 'h', 'e', 'r', 'n', 'o', '\0'};
 
+    using namespace std::string_literals;
+    std::string name0 = "Cherno"s + "hello";
+
+
     std::cout << name << std::endl;
     std::cout << name2 << std::endl;
+    std::cout << name0 << std::endl;
 
     std::cin.get();
 }
