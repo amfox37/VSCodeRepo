9cb86fd4375e39494ff2e8d1e059b8bcf69aa981
amfox37 <amfox37@gmail.com>
2020-09-16 19:24:36 -0600

    Need in to intialize to 0

--- projects/cherno_tutorials/video_25_constructors.cpp
+++ projects/cherno_tutorials/video_25_constructors.cpp
@@ -9,11 +9,19 @@ class Entity
     {
         std::cout << X << ", " << Y << std::endl;
     }
+
+    void Init()
+    {
+        X = 0.0f;
+        Y = 0.0f;
+    }
 };
 
 int main()
 {
     Entity e;
+    e.Init();
+    std::cout << e.X << std::endl;
     e.Print();
     std::cin.get();
 }
