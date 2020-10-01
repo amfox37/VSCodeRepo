b62efb522d8694b4753f6f857704cbc1065ca6dc
amfox37 <amfox37@gmail.com>
2020-09-30 20:29:38 -0600

    Don't do it like this, over writing object

--- projects/cherno_tutorials/video_36_member_initializer_list.cpp
+++ projects/cherno_tutorials/video_36_member_initializer_list.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include <string>
+class Example
+{
+    public:
+    Example()
+    {
+        std::cout << "Created Entity!" << std::endl;
+    }
+    
+    Example(int x)
+    {
+        std::cout << "Created Entity with" << x << "!" << std::endl;
+    }
+};
+
 
 class Entity
 {
     private:
     std::string m_Name;
+    Example m_Example;
     public:
     Entity()
     {
         m_Name = "Unknown";
+        m_Example = Example(8);
     }
 
     Entity(const std::string& name)
+    :         m_Name(name)
     {
-        m_Name = name;
     }
 
 const std::string& GetName() const { return m_Name; }
@@ -22,10 +38,6 @@ const std::string& GetName() const { return m_Name; }
 int main()
 {
     Entity e0;
-    std::cout << e0.GetName() << std::endl;
-
-    Entity e1("Andy");
-    std::cout << e1.GetName() << std::endl;
 
     std::cin.get();
 }
