91301531fc73688761520b70f60f40d985531ba7
amfox37 <amfox37@gmail.com>
2020-10-04 15:41:20 -0600

    video 40 second commit

--- projects/cherno_tutorials/video_38_instantiate.dSYM/Contents/video_40_explicit_keyword.cpp
+++ projects/cherno_tutorials/video_38_instantiate.dSYM/Contents/video_40_explicit_keyword.cpp
@@ -8,15 +8,17 @@ class Entity
     int m_Age;
 
     public:
-    Entity(const std::string& name) //"string reference"
+    explicit Entity(const std::string& name) //"string reference"
         : m_Name(name), m_Age(-1) {}
-    Entity(int age)
+    
+    explicit Entity(int age)
         : m_Name("unknown"), m_Age(age) {}
 };
 
 int main()
 {
     Entity a("Andy");
+    Entity a = "Andy"; // Fails with explicit keyword used
     Entity b(45);
 
     std::cin.get();
