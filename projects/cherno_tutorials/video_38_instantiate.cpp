1cc2bde74116c9db697340ab109f6961bdcb2001
amfox37 <amfox37@gmail.com>
2020-10-04 13:49:42 -0600

    video 38 first commit

---
+++ projects/cherno_tutorials/video_38_instantiate.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using String = std::string;
+
+class Entity
+{
+    private:
+    String m_Name;
+    public:
+    Entity() : m_Name("unknown") {}
+    Entity(const String& name) : m_Name(name) {}
+
+    const String& GetName() const { return m_Name; }
+};
+
+int main()
+{
+    Entity entity;
+    std::cout << entity.GetName() << std::endl;
+
+    std::cin.get();
+}
