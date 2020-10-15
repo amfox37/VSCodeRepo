5520a642d24dfa13781a91f0e1f62664c6275646
amfox37 <amfox37@gmail.com>
2020-10-14 18:33:57 -0600

    This is how you'd do it without overloading anything

--- projects/cherno_tutorials/video_41_operators.cpp
+++ projects/cherno_tutorials/video_41_operators.cpp
@@ -1,3 +1,6 @@
+// Operators are just functions
+// Overloading should be minimal and make perfect sense
+
 #include <iostream>
 #include <string>
 
@@ -7,12 +10,26 @@ struct Vector2
 
     Vector2(float x, float y)
         : x(x), y(y) {}
+
+    Vector2 Add(const Vector2& other) const
+    {
+        return Vector2(x + other.x, y +other.y);
+    }
+
+    Vector2 Multiply(const Vector2& other) const
+    {
+        return Vector2(x * other.x, y * other.y);
+    }
+
 };
 
 int main()
 {
     Vector2 position(4.0f, 4.0f);
     Vector2 speed(0.5f, 1.5f);
+    Vector2 powerup(1.1f, 1.1f);
+
+    Vector2 result = position.Add(speed.Multiply(powerup));
 
     std::cin.get();
 }
