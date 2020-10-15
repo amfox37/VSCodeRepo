0d75c6706c9210536719b70db3521db36f9a629c
amfox37 <amfox37@gmail.com>
2020-10-14 18:50:22 -0600

    Some overload operators, but I'm not sure about the stream stuff <<

--- projects/cherno_tutorials/video_41_operators.cpp
+++ projects/cherno_tutorials/video_41_operators.cpp
@@ -16,12 +16,28 @@ struct Vector2
         return Vector2(x + other.x, y +other.y);
     }
 
+    Vector2 operator+(const Vector2& other) const
+    {
+        return Add(other);
+    }
+
     Vector2 Multiply(const Vector2& other) const
     {
         return Vector2(x * other.x, y * other.y);
     }
 
+    Vector2 operator*(const Vector2& other) const
+    {
+        return Multiply(other);
+    }
+
 };
+
+std::ostream& operator<<(std::ostream& stream, const Vector2& other)
+{
+    stream << other.x << ", " << other.y;
+    return stream;
+}
 
 int main()
 {
@@ -29,7 +45,11 @@ int main()
     Vector2 speed(0.5f, 1.5f);
     Vector2 powerup(1.1f, 1.1f);
 
-    Vector2 result = position.Add(speed.Multiply(powerup));
+    Vector2 result1 = position.Add(speed.Multiply(powerup));
+
+    Vector2 result2 = position + speed * powerup;
+
+    std::cout << result2 << std::endl;
 
     std::cin.get();
 }
