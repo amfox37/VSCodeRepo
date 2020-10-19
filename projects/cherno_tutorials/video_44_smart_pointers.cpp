0dc9b4fe262b72165c959f6ca9d23de717edb7ff
amfox37 <amfox37@gmail.com>
2020-10-18 18:30:11 -0600

    Video 44 with unique pointers

---
+++ projects/cherno_tutorials/video_44_smart_pointers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+class Entity
+{
+    public:
+    Entity() //Constructer
+    {
+        std::cout << "Created Entity!" << std::endl;
+    }
+
+    ~Entity() // Deconstructer 
+    {
+        std::cout << "Destroyed Entity!" << std::endl;
+    }
+
+    void Print() {} // A dummy thing
+
+};
+
+int main()
+{
+    {
+        std::unique_ptr<Entity> entity = std::make_unique<Entity>();
+        entity ->Print();
+    }
+
+    std::cin.get();
+}
